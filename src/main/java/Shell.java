c067e6860af4b14d6cc95236659f3efec866ef4b
podlLev <pidlisnyilev@gmail.com>
2026-06-30 09:58:14 +0300

    feat: handle invalid commands

--- src/main/java/Shell.java
+++ src/main/java/Shell.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Shell {
 
     public static void main(String[] args) {
         System.out.print("$ ");
+
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        System.out.println(command + ": command not found");
     }
 
 }
